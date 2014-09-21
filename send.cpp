d9444067780789b68bedf817bccb2b53ba989415
rambow330 <1436835926@qq.com>
2014-09-21 22:57:17 +0800

    Create send.cpp

---
+++ send.cpp
@@ -0,0 +1,53 @@
+#include "send.h"
+#include "ui_send.h"
+#include "chat.h"
+#include "mainwindow.h"
+
+Send::Send(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Send)
+{
+    ui->setupUi(this);
+    setWindowFlags(Qt::FramelessWindowHint);//去掉标题栏
+
+    QPalette palette(this->palette());
+    palette.setColor(QPalette::Background, Qt::yellow);
+    this->setPalette(palette);
+
+}
+
+Send::~Send()
+{
+    delete ui;
+}
+
+//返回主界面
+void Send::on_returnButton_clicked()
+{
+    this->close();
+    MainWindow *w=new MainWindow();
+    w->show();
+
+}
+//定时短信
+void Send::on_timeButton_clicked()
+{
+
+}
+//取消定时
+void Send::on_notimeButton_clicked()
+{
+
+}
+//发送短信
+void Send::on_sendButton_clicked()
+{
+    this->close();
+    Chat *w=new Chat();
+    w->show();
+}
+//添加多个联系人
+void Send::on_addButton_clicked()
+{
+
+}
