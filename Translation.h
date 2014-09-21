37878bd2381d1735dea10bcc21d3b893476244b9
alexliu0809 <alexliu0809@hotmail.com>
2014-09-21 14:21:19 +0800

    Create Translation.h

---
+++ Translation.h
@@ -0,0 +1,22 @@
+#ifndef _TRANSLATION_H_
+#define _TRANSLATION_H_
+#include "SystemHead.h"
+#include "Message.h"
+#include "User.h"
+class TranslationHandler
+{
+public:
+	static string messageToString(Message o)
+	{
+		string a;
+		return a;
+	}
+	static Message stringToMessage(string o)
+	{
+		Message p;
+		return p;
+	}
+};
+
+
+#endif
