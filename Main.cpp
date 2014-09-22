f6380d95eaac63695fac4e5e5a0417b4104ca0d1
alexliu0809 <alexliu0809@hotmail.com>
2014-09-22 21:13:23 +0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -8,8 +8,8 @@ int num,message_num;
 string key[10000][4];
 
 
-//需要调试的部分：服务器端关闭后数据没有写入，导致第一次注册后关闭服务端第二次显示没有注册！
-//MessageHandler只能存用户名，CommunicationHandler只能存IP
+//需要调试的部分：服务器端关闭后数据没有写入，导致第一次注册后关闭服务端第二次显示没有注册！需要改的地方：CommunicationHandler里的send,OfflineMessageHandler的Get.
+//MessageHandler只能存用户名，CommunicationHandler只能存IP。
 int main()
 {
 	Server myServer;
