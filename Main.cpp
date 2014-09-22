de060cb107cc3fcd6ec855c725a3a863bfb333b4
alexliu0809 <alexliu0809@hotmail.com>
2014-09-22 18:08:32 +0800

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,21 @@
+#include "Server.h"
+#include "Database.h"
+int head,rear;
+string msgToBeDealed[1000];//string数组
+CRITICAL_SECTION critical;
+int num,message_num;
+string key[10000][4];
+
+
+
+
+int main()
+{
+	Server myServer;
+	head=rear=0;
+	InitializeCriticalSection(&critical);
+	Database *myDatabase=new Database();
+	myServer.startServer();
+
+	
+}
