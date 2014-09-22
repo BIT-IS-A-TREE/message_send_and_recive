3862c23163d2d810c77c5df14b5a8e1f57dd0679
alexliu0809 <alexliu0809@hotmail.com>
2014-09-22 21:04:22 +0800

    Update OfflineMessageHandler.h

--- OfflineMessageHandler.h
+++ OfflineMessageHandler.h
@@ -6,18 +6,29 @@
 
 class OfflineMessageHandler{
 
-
+private:
+	list<Message>  myOfflineMessageList;
 
 
 public:
-	queue<Message> getMessage(char IP[])
+	queue<Message> getMessage(char Name[])//不能传ip要传用户名
 	{
-		queue<Message> IPMessage;//属于这个IP的短信
-		return IPMessage;
+		queue<Message> NameMessage;//属于这个IP的短信
+		list<Message>::iterator it;
+		for (it=myOfflineMessageList.begin();it!=myOfflineMessageList.end();it++)
+		{
+			if (strcmp(it->receiver,Name)==0)
+			{
+				NameMessage.push(*it);
+			}
+		}
+
+		//待完善。
+		return NameMessage;
 	}
 	void addMessage(Message o)
 	{
-
+		myOfflineMessageList.push_back(o);
 	}
 };
 
