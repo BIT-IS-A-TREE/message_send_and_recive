82dfec7fd68560d5580c3dce827bcb6c9085d3c2
alexliu0809 <alexliu0809@hotmail.com>
2014-09-22 18:50:35 +0800

    Update SystemHead.h

--- SystemHead.h
+++ SystemHead.h
@@ -6,9 +6,18 @@
 #include <stdio.h>
 //#pragma comment(lib,"ws2_32.lib")
 #pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib,"libmysql") 
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <process.h> 
+#include <string.h> 
+#include <stdlib.h> 
+#include <stdio.h> 
+#include <winsock2.h> 
+#include <mysql.h>/*注意要包含这个头文件*/ 
+#include <string>
+#include<cstdio>
+#include<ctime>
 //#include "Message.h"
 //#include "User.h"
 #include <WINSOCK2.H>
