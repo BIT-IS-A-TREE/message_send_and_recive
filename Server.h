4cedb629613d274648ddcc984bee5c89f7569170
alexliu0809 <alexliu0809@hotmail.com>
2014-09-22 10:50:29 +0800

    Update Server.h

--- Server.h
+++ Server.h
@@ -23,36 +23,123 @@ private:
 	static void Decode(string o)//这个地方都是要改的，
 	{
 		//比如说第一个是发送。
-		if (strcmp(o.c_str(),"Online")==0)//上线
-		{
-			char arg[1000];//假设就是Ip
-			strcpy(arg,o.c_str());
-			queue<Message> tempQueue=myOfflineMessageHandler.getMessage(arg);
-			CommunicationHandler CM=myCommunicationHandlerList.getHandler(arg);
-			while (!tempQueue.empty())
-			{
-				CM.sendInformation(tempQueue.front());
-				tempQueue.pop();
-			}
-		}
-		if (strcmp(o.c_str(),"Send")==0)//发送
-		{
-			if (strcmp(o.c_str(),"SetTime")==0)//定时短信。
-			{
-				;
-			}
+		//if (strcmp(o.c_str(),"Online")==0)//上线
+		//{
+		//	char arg[1000];//假设就是Ip
+		//	strcpy(arg,o.c_str());
+		//	queue<Message> tempQueue=myOfflineMessageHandler.getMessage(arg);
+		//	CommunicationHandler CM=myCommunicationHandlerList.getHandler(arg);
+		//	while (!tempQueue.empty())
+		//	{
+		//		CM.sendInformation(tempQueue.front());
+		//		tempQueue.pop();
+		//	}
+		//}
+		//if (strcmp(o.c_str(),"Send")==0)//发送
+		//{
+		//	if (strcmp(o.c_str(),"SetTime")==0)//定时短信。
+		//	{
+		//		;
+		//	}
 
 
-			//int CheckOnline=check();
-			//if (online)
-			//{
-			//	send;
-			//}
-			//else
-			//{
-			//	myOfflienMessageHandler.addMessage();
-			//}
+		//	//int CheckOnline=check();
+		//	//if (online)
+		//	//{
+		//	//	send;
+		//	//}
+		//	//else
+		//	//{
+		//	//	myOfflienMessageHandler.addMessage();
+		//	//}
+		//}
+
+
+
+
+		char tempString[1000];
+		strcpy(tempString,o.c_str());
+		char *token=strtok(tempString,"@");
+		
+		//短信加解密顺序：Message@是否定时@定时时间@收件人@发件人@发送标识@短信标识@长短信顺序标识@收发时间@内容   如果不是定时短信定时时间可以随便设定，反正最后也不会调用这个属性
+		if (strcmp(token,"Message")==0)//表示是短信
+		{
+			Message o;
+			token = strtok (NULL, "@");
+			if (strcmp(token,"0")==0)//非定时
+			{
+				o.IssetTime=0;//是否定时
+				token = strtok (NULL, "@");
+				o.setTime=atoi(token);//定时时间
+				token = strtok (NULL, "@");
+				strcpy(o.receiver,token);//收件人
+				token = strtok (NULL, "@");
+				strcpy(o.sender,token);//发件人
+				token = strtok (NULL, "@");
+				o.sendRceiveFlag=atoi(token);//发送标识
+				token = strtok (NULL, "@");
+				o.lmesFlag=atoi(token);//短信标识
+				token = strtok (NULL, "@");
+				o.lmesSt=atoi(token);//长短信顺序标识
+				token = strtok (NULL, "@");
+				o.time=atoi(token);//收发时间
+				token =strtok(NULL,"@");
+				string temp="";
+				while (token!=NULL)
+				{
+					temp+=string(token);
+					token =strtok(NULL,"@");
+				}//把内容都存起来
+				strcpy(o.content,temp.c_str());
+				//短信内容保存完毕！
+				
+				//在线！查找CM,发送
+				//不在线，调出OFFline，加入
+
+
+			}
+			else if (strcmp(token,"1")==0)//定时
+			{
+				o.IssetTime=1;//是否定时
+				token = strtok (NULL, "@");
+				o.setTime=atoi(token);//定时时间
+				token = strtok (NULL, "@");
+				strcpy(o.receiver,token);//收件人
+				token = strtok (NULL, "@");
+				strcpy(o.sender,token);//发件人
+				token = strtok (NULL, "@");
+				o.sendRceiveFlag=atoi(token);//发送标识
+				token = strtok (NULL, "@");
+				o.lmesFlag=atoi(token);//短信标识
+				token = strtok (NULL, "@");
+				o.lmesSt=atoi(token);//长短信顺序标识
+				token = strtok (NULL, "@");
+				o.time=atoi(token);//收发时间
+				token =strtok(NULL,"@");
+				string temp="";
+				while (token!=NULL)
+				{
+					temp+=string(token);
+					token =strtok(NULL,"@");
+				}//把内容都存起来
+				strcpy(o.content,temp.c_str());
+				//短信内容保存完毕！
+
+
+				//加入到定时短信队列！
+				mySetTimeMessageHandler.addMessage(o);
+
+			}
+			else
+			{
+				printf("解析定时时报错！\n");
+				return ;
+			}
+			return ;
 		}
+	//	if (strcmp(token,""))
+
+
 		
 	
 	}
