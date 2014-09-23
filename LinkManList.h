8f9cc54b13b1363ef799ba03d5445f962720db1f
qinhuan <1300263075@qq.com>
2014-09-23 15:17:59 +0800

    Update LinkManList.h

--- LinkManList.h
+++ LinkManList.h
@@ -1,13 +1,21 @@
-#include"LinkMan.h"
+#include<time.h>
 
+struct LinkMan
+{
+    char phonenumber[15];//手机号
+    int mes_num;//短信数目
+    time_t time;//该联系人的最新一条信息的时间,若无信息则置为0
+};
 
 class LinkManList{
 public:
     LinkMan linkManListTime[100];//按联系人最后短信时间排序
 	int linkManNum;//联系人个数
+    int nowLinkMAn;//正在查看的联系人
 public:
-    bool searchLinkMan(char *);//查询是否有该手机号；若有返回true否则返回false
+    bool isContain(char *str1,char *str2);//str1为子串，str2为母串，判断母串中是否有子串
 	void creatLinkManList();//创建
-	void addLinkMan(LinkMan);//插入新号码
+    void addLinkMan(LinkMan);//插入新联系人
     void sortList();//重新进行排序
+    void delateLinkMan(int st);//删除第st个LinkMan
 };
