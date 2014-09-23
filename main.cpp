811e065b4a8d37de1244b1b7a11f4fe5f6d59f43
qinhuan <1300263075@qq.com>
2014-09-23 15:15:48 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,22 +3,15 @@
 #include <QWidget>
 #include <QDialog>
 #include "login.h"
-#include <QtGui>
-#include <QFrame>
-#include <QPalette>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    Login login;
 
-    if(login.exec()==QDialog::Accepted)
-    {
-        w.show();
-        return a.exec();
-    }
-    else
-    return 0;
+    Login *login=new Login();
+    login->show();
+
+   return a.exec();
+
 
 }
