e41e6a13144f6f6d533d94005f366b5b0eda99c8
alexliu0809 <alexliu0809@hotmail.com>
2014-09-23 12:38:26 +0800

    Update Translation.h

--- Translation.h
+++ Translation.h
@@ -8,14 +8,26 @@ class TranslationHandler
 public:
 	static string messageToString(Message o)
 	{
-		string a;
-		return a;
+		string temp;
+		temp="Message@";
+		temp+=o.IssetTime +"@";
+		temp+=o.setTime+'@';
+		temp+=o.receiver+'@';
+		temp+=o.sender+'@';
+		temp+=o.sendRceiveFlag+'@';
+		temp+=o.lmesFlag+'@';
+		temp+=o.lmesSt+'@';
+		temp+=o.time+'@';
+		temp+=o.content;
+		return temp;
 	}
-	static Message stringToMessage(string o)
+	static Message stringToMessage(string &o)
 	{
-		Message p;
-		return p;
+		Message a;
+		return a;
+		
 	}
+	
 };
 
 
